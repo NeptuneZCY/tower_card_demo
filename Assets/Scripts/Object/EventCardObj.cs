212760c65f989714b035615f2096ca1166f6715e
Netune <345111782@qq.com>
2025-01-04 14:13:32 +0800

    bugfix

---
+++ Assets/Scripts/Object/EventCardObj.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class EventCardObj
+{
+    public string 序号;
+    public string 放入形状;
+    public string 描述内容;
+    public string 属性影响1;
+    public string 属性影响2;
+    public string 属性影响3;
+    public string 属性影响4;
+    public string 属性影响5;
+    public string 属性影响6;
+}
+
+// List<T>
+[Serializable]
+public class Serialization<T>
+{
+    [SerializeField]
+    List<T> target;
+    public List<T> ToList() { return target; }
+
+    public Serialization(List<T> target)
+    {
+        this.target = target;
+    }
+}
+
+// Dictionary<TKey, TValue>
+[Serializable]
+public class Serialization<TKey, TValue> : ISerializationCallbackReceiver
+{
+    [SerializeField]
+    List<TKey> keys;
+    [SerializeField]
+    List<TValue> values;
+
+    Dictionary<TKey, TValue> target;
+    public Dictionary<TKey, TValue> ToDictionary() { return target; }
+
+    public Serialization(Dictionary<TKey, TValue> target)
+    {
+        this.target = target;
+    }
+
+    public void OnBeforeSerialize()
+    {
+        keys = new List<TKey>(target.Keys);
+        values = new List<TValue>(target.Values);
+    }
+
+    public void OnAfterDeserialize()
+    {
+        var count = Math.Min(keys.Count, values.Count);
+        target = new Dictionary<TKey, TValue>(count);
+        for (var i = 0; i < count; ++i)
+        {
+            target.Add(keys[i], values[i]);
+        }
+    }
+}
